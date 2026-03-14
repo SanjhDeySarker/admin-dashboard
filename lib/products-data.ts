8e8bd8396a29339546d0b70d3fe4eff3f49f425e
Sanjh Dey Sarker <sanjhdeysarker@gmail.com>
2026-03-14 13:17:37 +0530

    products-data-add

---
+++ lib/products-data.ts
@@ -0,0 +1,16 @@
+export const products = [
+  {
+    id: 1,
+    name: "Laptop",
+    category: "Electronics",
+    price: "$1200",
+    stock: 20,
+  },
+  {
+    id: 2,
+    name: "Headphones",
+    category: "Accessories",
+    price: "$200",
+    stock: 50,
+  },
+];
