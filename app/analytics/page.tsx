a060563b99ebff6664b84ff8261f90f915a030cd
Sanjh Dey Sarker <sanjhdeysarker@gmail.com>
2026-03-26 14:31:13 +0530

    minimal-changes

---
+++ app/analytics/page.tsx
@@ -0,0 +1,9 @@
+export default function AnalyticsPage() {
+    return (
+        <div>
+            <h1>Analytics Page</h1>
+            <p>This page is used for analytics purposes.</p>
+        </div>
+    );
+}
+
